1420bab1d1d78d57a1344cfb75b77138d5deac85
a <35520491+gorshco@users.noreply.github.com>
2021-06-10 09:29:05 +0300

    Add files via upload

---
+++ content.js
@@ -0,0 +1,19 @@
+function hideResults() {
+    // select all search results (with "div.g" selector)
+    var searchResults = document.querySelectorAll('div.g');
+
+    // iterate through all search results
+    for (var i = 0; i < searchResults.length; i++) {
+
+        // try matching "static1.squarespace.com" in the search result URL
+        matchedResult = searchResults[i].querySelector('a[href^="https://static1.squarespace.com/"], a[href^="http://static1.squarespace.com/"]');
+
+        // if "static1.squarespace.com" was found in the search result URL
+        if (matchedResult) {
+
+            // hide search result
+            searchResults[i].style.display = "none";
+        }
+    }
+}
+hideResults()
